7acb3abc364d770a1bec2b8a562b15c5584cc666
av164702gmail.com <av164702gmail.com>
2025-08-26 16:26:00 -0500

    inicio del proyecto

---
+++ front/src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a Tecnova</h1>
+      <p>Esta es la página de inicio de tu aplicación.</p>
+    </div>
+  );
+}
+
+export default Home;
